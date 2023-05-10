89e1a0d21dcb9e766b2ec874aa208cc7295e7dc9
FritoMatt <78970996+FritoMatt@users.noreply.github.com>
2023-05-10 01:07:29 -0400

    Added custom fonts and font sizes

--- HorseRacingGame.java
+++ HorseRacingGame.java
@@ -9,7 +9,7 @@ public class HorseRacingGame extends JFrame {
     private final JButton startButton = new JButton("Play Again");
     private final JTextField betField = new JTextField(10);
     private final JLabel betHere = new JLabel("<-- Bet Here");
-    
+
     private int winner;
 
     public HorseRacingGame() {
@@ -18,16 +18,21 @@ public class HorseRacingGame extends JFrame {
         setLayout(new FlowLayout());
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         getContentPane().setBackground(Color.green);
-        
+
+        Font font = new Font("Times New Roman", Font.PLAIN, 16);
+
         ImageIcon icon = new ImageIcon("HorseLogo.png"); // Image needs to be in project folder.
         setIconImage(icon.getImage());
-        JPanel horsePanel = new JPanel(new GridLayout(4, 2)); //setup the "racetrack"
-        for (int i = 0; i < 4; i++) {//runs arrays until 4
+        JPanel horsePanel = new JPanel(new GridLayout(4, 2)); // setup the "racetrack"
+        for (int i = 0; i < 4; i++) {// runs arrays until 4
+
             JLabel horseLabel = new JLabel("Horse " + (i + 1));
+            horseLabel.setFont(font);
             horseLabels[i] = horseLabel;
             horsePanel.add(horseLabel);
 
             JLabel progressLabel = new JLabel();
+            horseLabel.setFont(font);
             progressLabels[i] = progressLabel;
             horsePanel.add(progressLabel);
         }
@@ -36,37 +41,38 @@ public class HorseRacingGame extends JFrame {
         JPanel controlPanel = new JPanel(new FlowLayout());
         controlPanel.add(startButton);
         controlPanel.add(betField);
+        betField.setFont(font);
         controlPanel.add(betHere);
-        
+        betHere.setFont(font);
+
         add(controlPanel, BorderLayout.SOUTH);
 
         startButton.addActionListener(e -> startRace());
-        
+        startButton.setFont(font);
 
         setVisible(true);
     }
 
-    private void startRace() {//method for the horses moving
+    private void startRace() {// method for the horses moving
         startButton.setEnabled(false);
-        
 
-        Random random = new Random(); //this is the random for the positions of the horses
-        int[] positions = new int[4]; //position array for the horses
+        Random random = new Random(); // this is the random for the positions of the horses
+        int[] positions = new int[4]; // position array for the horses
 
         int winner = -1; // store the winner horse's number
-        boolean finished = false; //this boolean stops the horses from moving when true
-        while (!finished) {//runs when not finished
-            for (int j = 0; j < 4; j++) {//this takes care of the random positioning of the sprites
+        boolean finished = false; // this boolean stops the horses from moving when true
+        while (!finished) {// runs when not finished
+            for (int j = 0; j < 4; j++) {// this takes care of the random positioning of the sprites
                 int newPosition = positions[j] + random.nextInt(3) + 1;
                 if (newPosition > 50) {
                     newPosition = 50;
                 }
                 positions[j] = newPosition;
                 updateProgress(j, positions[j]);
-                
-                if (positions[j] == 50) {//when the first horse to finish it makes it winner 
+
+                if (positions[j] == 50) {// when the first horse to finish it makes it winner
                     winner = j;
-                    finished = true;//when first place finishes all horses stop
+                    finished = true;// when first place finishes all horses stop
                     break;
                 }
             }
@@ -81,19 +87,20 @@ public class HorseRacingGame extends JFrame {
 
         // calls placeBet with the winner horse's number
         placeBet(winner + 1);
-        
+
         startButton.setEnabled(true);
-        
+
     }
 
-    private void placeBet(int winnerHorse) { //this method tells the user whether their bet was correct or not
-        try {//all of these statements check if input is correct or whether their bet was any good
+    private void placeBet(int winnerHorse) { // this method tells the user whether their bet was correct or not
+        try {// all of these statements check if input is correct or whether their bet was
+             // any good
             int bet = Integer.parseInt(betField.getText());
             if (bet < 1 || bet > 4) {
                 JOptionPane.showMessageDialog(this, "Invalid bet! Choose a horse number between 1 and 4.");
                 return;
             }
-            if (bet != winnerHorse) { 
+            if (bet != winnerHorse) {
                 JOptionPane.showMessageDialog(this, "You lose!");
             } else {
                 JOptionPane.showMessageDialog(this, "You win!");
@@ -103,9 +110,10 @@ public class HorseRacingGame extends JFrame {
         }
     }
 
-    private void updateProgress(int horseIndex, int position) {//this method is called in the startrace method and used to update the position of the horses. 
+    private void updateProgress(int horseIndex, int position) {// this method is called in the startrace method and used
+                                                               // to update the position of the horses.
         StringBuilder builder = new StringBuilder();
-        for (int i = 1; i <= 50; i++) {//array 50 represents the number of posistions on the race track
+        for (int i = 1; i <= 50; i++) {// array 50 represents the number of posistions on the race track
             if (i == position) {
                 builder.append(horseIndex + 1);
             } else {
@@ -114,9 +122,24 @@ public class HorseRacingGame extends JFrame {
         }
         progressLabels[horseIndex].setText(builder.toString());
     }
-    public static void main(String[] args) { 
-        HorseRacingGame game = new HorseRacingGame();
 
+    public static void main(String[] args) {
+        // Set font and size for the UI
+        Font font = new Font("Times New Roman", Font.PLAIN, 16);
+        UIManager.put("OptionPane.messageFont", font);
+
+        SwingUtilities.invokeLater(() -> {
+            try {
+                // Set look and feel to the system's default
+                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        
+        
+            
+        });
+        HorseRacingGame game = new HorseRacingGame();
         // tells user to place a bet before starting the race
         int bet = 0;
         //runs if there bet is between 1-4 
@@ -134,7 +157,7 @@ public class HorseRacingGame extends JFrame {
             } catch (NumberFormatException e) {
                 JOptionPane.showMessageDialog(game, "Invalid bet! Enter a number between 1 and 4.");
             }
-        }
+        
 
         //place the bet
         game.betField.setText(Integer.toString(bet));
@@ -142,5 +165,7 @@ public class HorseRacingGame extends JFrame {
         //start the race
         game.startRace();
         // game.placeBet(bet);
+        
     }
+}
 }
